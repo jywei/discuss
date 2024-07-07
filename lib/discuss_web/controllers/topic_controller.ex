b7dac78ac4ccb46dbc99215f5b8a1317b847c299
Roy Wei <roywjy@gmail.com>
2024-07-07 16:46:05 +1000

    Add a comment for "use DiscussWeb, :controller"

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,5 +1,18 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
+  # lib/discuss_web.ex
+  # def controller do
+  #   quote do
+  #     use Phoenix.Controller,
+  #       formats: [:html, :json],
+  #       layouts: [html: DiscussWeb.Layouts]
+
+  #     import Plug.Conn
+  #     import DiscussWeb.Gettext
+
+  #     unquote(verified_routes())
+  #   end
+  # end
 
   def new() do
 
