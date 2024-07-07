4808fb0de328b8779e0521b3cefbe0e2f708356b
Roy Wei <roywjy@gmail.com>
2024-07-07 20:23:28 +1000

    Add html

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,9 +1,11 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
+
   alias Discuss.Topics.Topic
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
+
     render(conn, :new, changeset: changeset)
   end
 end
